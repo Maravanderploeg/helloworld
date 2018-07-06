a9f61eb80daeffcdb1ea814072c0e7bda31f9dc9
Mircea Filip Lungu <mircea.lungu@gmail.com>
2018-07-06 09:25:09 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -6,7 +6,10 @@ german = [
     
     #http://www.faz.net/aktuell/wirtschaft/amerikanische-iran-sanktionen-welchen-schutz-gibt-es-15582815.html
     "Das trifft vor allem auf Konzerne aus dem Energiesektor zu.", 
-    "Das schließt auch Geldtransfers in Dollar aus."
+    "Das schließt auch Geldtransfers in Dollar aus.",
+
+    # http://www.spiegel.de/kultur/tv/das-boot-das-parfum-die-protokollantin-servus-baby-neue-deutsche-serien-a-1216797.html
+    "Aber auch viel Geld verdienen, und deswegen taucht 'Das Boot' 37 Jahre nach der Premiere bald wieder auf, voraussichtlich im November: als neue Serie für den Bezahlsender Sky, größer, aufwendiger, teurer - acht Teile, gedreht an 105 Tagen in vier Ländern, mit über 25 Millionen Euro Budget, vom exzellenten österreichischen Regisseur Andreas Prochaska (Das finstere Tal) inszeniert."
 ]
 
 articles = [
